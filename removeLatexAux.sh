f0bf160bbdd163db55c2adb15762f358bbe5ae30
Just-Danuse <danielzidek@post.cz>
2023-09-22 04:41:53 +0200

    Add technical documentation to docs

--- removeLatexAux.sh
+++ removeLatexAux.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-cd "Docs/Developer notes/"
+cd "Docs/DevNotes/"
 
-ls *.{glsdefs,ist,run.xml,synctex.gz,aux,bbl,blg,idx,ind,lof,lot,out,toc,acn,acr,alg,glg,glo,gls,fls,log,fdb_latexmk,snm,"synctex(busy)","synctex.gz(busy)",nav}
+rm *.{glsdefs,ist,run.xml,synctex.gz,aux,bbl,blg,idx,ind,lof,lot,out,toc,acn,acr,alg,glg,glo,gls,fls,log,fdb_latexmk,snm,"synctex(busy)","synctex.gz(busy)",nav}
+rm main-blx.bib
 
-read -p "Pokračovat? " -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[YyAa]$ ]]
-then
-    rm *.{glsdefs,ist,run.xml,synctex.gz,aux,bbl,blg,idx,ind,lof,lot,out,toc,acn,acr,alg,glg,glo,gls,fls,log,fdb_latexmk,snm,"synctex(busy)","synctex.gz(busy)",nav}
-    rm main-blx.bib
-fi
+cd "Docs/TechDoc/"
+
+rm *.{glsdefs,ist,run.xml,synctex.gz,aux,bbl,blg,idx,ind,lof,lot,out,toc,acn,acr,alg,glg,glo,gls,fls,log,fdb_latexmk,snm,"synctex(busy)","synctex.gz(busy)",nav}
+rm main-blx.bib
